5f89ec1af99dae08997dd29ff4abfda4b759bf5d
sarfraz <eng.sarfraz@gmail.com>
2018-06-24 23:14:44 +0500

    Page URL added

--- Selenium-CSharp/Selenium-CSharp/PageObjects_LogIn.cs
+++ Selenium-CSharp/Selenium-CSharp/PageObjects_LogIn.cs
@@ -17,22 +17,30 @@ namespace Selenium_CSharp
 
             PageFactory.InitElements(propertiesCollection.driver, this);
 
-        }
+        }   
 
 
         #region Login Page - Web Elements
 
         //User Name
         [FindsBy(How = How.Id , Using = "txtUsername")]
+        [CacheLookup]
         private IWebElement tbUserName { get; set; }
 
         //Password
         [FindsBy(How = How.Id, Using = "txtPassword")]
+        [CacheLookup]
         private IWebElement tbPassword { get; set; }
 
         //Submit Button
         [FindsBy(How = How.Name, Using = "Submit")]
+        [CacheLookup]
         private IWebElement btnSubmit { get; set; }
+
+
+        public readonly string pageLoadedText = "";
+
+        public readonly string pageUrl = "http://opensource.demo.orangehrmlive.com/";
 
         #endregion
 
